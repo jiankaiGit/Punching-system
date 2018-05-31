a7c61d975e06ade9236c1b8cc0b010916ea9bc43
chang jiankai <qwdf4209@gmail.com>
2018-05-31 22:17:16 +0800

    first version

---
+++ clock-In/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace clock_In
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.clockBtn = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.addNew = new System.Windows.Forms.Button();
+            this.employeeState = new System.Windows.Forms.Label();
+            this.dataInfo = new System.Windows.Forms.ListBox();
+            this.nameInputCB = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // nameInput
+            // 
+            this.nameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInput.Location = new System.Drawing.Point(22, 68);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(100, 35);
+            this.nameInput.TabIndex = 1;
+            this.nameInput.Text = "輸入性名";
+            // 
+            // clockBtn
+            // 
+            this.clockBtn.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuItem;
+            this.clockBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clockBtn.Location = new System.Drawing.Point(128, 173);
+            this.clockBtn.Name = "clockBtn";
+            this.clockBtn.Size = new System.Drawing.Size(192, 83);
+            this.clockBtn.TabIndex = 3;
+            this.clockBtn.Text = "打卡";
+            this.clockBtn.UseVisualStyleBackColor = true;
+            this.clockBtn.Click += new System.EventHandler(this.clockBtn_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.title.Location = new System.Drawing.Point(12, 10);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(122, 55);
+            this.title.TabIndex = 4;
+            this.title.Text = "興盛";
+            // 
+            // addNew
+            // 
+            this.addNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addNew.Location = new System.Drawing.Point(128, 68);
+            this.addNew.Name = "addNew";
+            this.addNew.Size = new System.Drawing.Size(192, 68);
+            this.addNew.TabIndex = 7;
+            this.addNew.Text = "加入新員工";
+            this.addNew.UseVisualStyleBackColor = true;
+            this.addNew.Click += new System.EventHandler(this.addNew_Click);
+            // 
+            // employeeState
+            // 
+            this.employeeState.AutoSize = true;
+            this.employeeState.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeState.Location = new System.Drawing.Point(140, 10);
+            this.employeeState.Name = "employeeState";
+            this.employeeState.Size = new System.Drawing.Size(61, 29);
+            this.employeeState.TabIndex = 8;
+            this.employeeState.Text = "狀態";
+            // 
+            // dataInfo
+            // 
+            this.dataInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataInfo.FormattingEnabled = true;
+            this.dataInfo.ItemHeight = 24;
+            this.dataInfo.Location = new System.Drawing.Point(326, 10);
+            this.dataInfo.Name = "dataInfo";
+            this.dataInfo.Size = new System.Drawing.Size(591, 268);
+            this.dataInfo.TabIndex = 10;
+            // 
+            // nameInputCB
+            // 
+            this.nameInputCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInputCB.FormattingEnabled = true;
+            this.nameInputCB.Location = new System.Drawing.Point(22, 173);
+            this.nameInputCB.Name = "nameInputCB";
+            this.nameInputCB.Size = new System.Drawing.Size(100, 37);
+            this.nameInputCB.TabIndex = 11;
+            this.nameInputCB.Text = "Name";
+            this.nameInputCB.SelectedIndexChanged += new System.EventHandler(this.nameInputCB_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 304);
+            this.Controls.Add(this.nameInputCB);
+            this.Controls.Add(this.dataInfo);
+            this.Controls.Add(this.employeeState);
+            this.Controls.Add(this.addNew);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.clockBtn);
+            this.Controls.Add(this.nameInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "興盛打卡系統";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Button clockBtn;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button addNew;
+        private System.Windows.Forms.Label employeeState;
+        private System.Windows.Forms.ListBox dataInfo;
+        private System.Windows.Forms.ComboBox nameInputCB;
+    }
+}
+
---
+++ clock-In/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clock_In
+{
+
+    public partial class Form1 : Form
+    {
+        public class EmployeeData
+        {
+            public string Date { get; set; }
+            public List<string> Time { get; set; }
+            public int TotalWorkTime { get; set; }
+        }
+
+        List<string> employeeList = new List<string>();
+        string type = "";
+        string selectedName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clockBtn_Click(object sender, EventArgs e)
+        {
+            string username = nameInput.Text;
+            string savePath = System.Windows.Forms.Application.StartupPath;
+            string dateAndTime = DateTime.Now.ToString(); // 2017-03-4 20:02:10
+            string date = DateTime.Now.ToString().Substring(0,9);
+            string time = DateTime.Now.TimeOfDay.ToString();
+            WriteClockRecord(savePath, selectedName,dateAndTime,date,time);
+        }
+
+        public void WriteClockRecord(string savePath, string name, string record, string date, string time)
+        {
+            EmployeeData empData = new EmployeeData();
+            List<string> tempTime = new List<string>();
+            string allTime = null;
+            string recordData = null;
+            Boolean dateExist = false;
+            if (checkNameExist(name))
+            {
+                string fullPath = savePath + "/紀錄/員工打卡紀錄/" + name + ".txt";
+                if (File.Exists(fullPath))
+                {
+                    StreamReader sr = new StreamReader(fullPath);
+
+                    string readLine;
+                    while ((readLine = sr.ReadLine()) != null)
+                    {
+                        if (readLine.Contains(date))
+                        {
+                            recordData = "," + time.Substring(0, 8);
+                            dateExist = true;
+                            Console.WriteLine(recordData);
+                        }
+                        else
+                        {
+                            recordData = "日期:" + date + "," + "時間:" + time.Substring(0, 8);
+                            dateExist = false;
+                            
+                        }
+                    }
+                    sr.Close();
+                    StreamWriter sw = new StreamWriter(fullPath, true);
+                    if (dateExist)
+                    {
+                        sw.Write(recordData);
+                    }
+                    else
+                    {
+                        sw.WriteLine(recordData);
+                    }
+
+
+                    sw.Flush();
+                    sw.Close();
+                    ReadEmployeeClockRecord(savePath, name);
+                }
+                else
+                {
+                    FileStream fs = new FileStream(fullPath, FileMode.Create);
+                    fs.Close();
+                    recordData = "日期:" + date + "," + "時間:" + time.Substring(0, 8);
+                    StreamWriter sw = new StreamWriter(fullPath, true);
+                    sw.Write(recordData);
+                    sw.Flush();
+                    sw.Close();
+                    ReadEmployeeClockRecord(savePath, name);
+
+                }
+                employeeState.Text = "打卡成功";
+                employeeState.ForeColor = Color.Green;
+            }
+            else
+            {
+                employeeState.Text = "員工不存在";
+                employeeState.ForeColor = Color.Red;
+            }
+
+        }
+
+        public void WriteNewEmpolyee(string savePath, string name)
+        {
+                string fullPath = savePath + "/紀錄/員工列表.txt";
+                FileStream fs = new FileStream(fullPath, FileMode.OpenOrCreate);
+                fs.Close();
+                StreamWriter sw = new StreamWriter(fullPath,true);
+                sw.WriteLine(name);
+                sw.Flush();
+                sw.Close();
+                nameInputCB.Items.Add(name);
+                employeeState.Text = "新增成功";
+                employeeState.ForeColor = Color.Green;
+
+
+        }
+
+        public void ReadEmployeeList(string path)
+        {
+            string fullPath = path + "/紀錄/員工列表.txt";
+            if (File.Exists(fullPath))
+            {
+                StreamReader sr = new StreamReader(fullPath);
+                String line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    employeeList.Add(line);
+                    nameInputCB.Items.Add(line);
+                    Console.WriteLine(line.ToString());
+                }
+                sr.Close();
+            }
+            else
+            {
+                employeeState.Text = "無員工";
+                employeeState.ForeColor = Color.Red;
+            }
+
+        }
+
+        public void ReadEmployeeClockRecord(string path,string name)
+        {
+            string fullPath = path + "/紀錄/員工打卡紀錄/" + name + ".txt";
+            string tempRecord = "";
+            string tmpTime = null;
+            DateTime dt1, dt2;
+            dataInfo.Items.Clear();
+            if (File.Exists(fullPath))
+            {
+                StreamReader sr = new StreamReader(fullPath);
+                String line;
+                string firstTime, lastTime, totalTime;
+                List<string> timeTemp = new List<string>();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    tmpTime = line.Substring(line.IndexOf("時間:")+3);
+                    if (tmpTime.Contains(','))
+                    {
+                        timeTemp = tmpTime.Split(',').ToList();
+                        firstTime = timeTemp[0];
+                        lastTime = timeTemp[timeTemp.Count()-1];
+                        dt1 = Convert.ToDateTime(firstTime);
+                        dt2 = Convert.ToDateTime(lastTime);
+                        TimeSpan ts = dt2 - dt1;
+                        tempRecord = line.Substring(0, 16)+ "上班: "+firstTime+",下班: "+lastTime+" 時數: "+ts.ToString().Substring(0,5);
+
+                    }
+                    else
+                    {
+                        tempRecord = line.Insert(16, "上班: ")+" 時數: 00:00";
+                    }
+
+                    Console.WriteLine(line.ToString());
+                    dataInfo.Items.Add(tempRecord);
+                }
+                sr.Close();
+                employeeState.Text = "";
+
+            }
+            else
+            {
+                employeeState.Text = "無打卡紀錄";
+                employeeState.ForeColor = Color.Red;
+            }
+
+        }
+
+        public Boolean checkNameExist(string name)
+        {
+            if (employeeList.Contains(name) && name != null && !name.Equals("輸入性名") &&  !name.Equals(""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void addNew_Click(object sender, EventArgs e)
+        {
+            string username = nameInput.Text;
+            if (checkNameExist(username))
+            {
+                employeeState.Text = "員工已加入，或名字相同!";
+                employeeState.ForeColor = Color.Red;
+            }
+            else
+            {
+                if (username == null || username.Equals("輸入性名") || username.Equals(""))
+                {
+                    employeeState.Text = "員工不存在";
+                    employeeState.ForeColor = Color.Red;
+                }
+                else
+                {
+                    string savePath = System.Windows.Forms.Application.StartupPath;
+                    employeeList.Add(username);
+                    WriteNewEmpolyee(savePath, username);
+                    employeeState.Text = "加入成功";
+                    employeeState.ForeColor = Color.Green;
+                }
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string savePath = System.Windows.Forms.Application.StartupPath;
+            if (!Directory.Exists(savePath + "/紀錄"))
+            {
+                Directory.CreateDirectory(savePath+"/紀錄");
+                Directory.CreateDirectory(savePath + "/紀錄/員工打卡紀錄");
+            }
+            ReadEmployeeList(savePath);
+        }
+
+        private void nameInputCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string savePath = System.Windows.Forms.Application.StartupPath;
+            selectedName = nameInputCB.SelectedItem.ToString();
+            ReadEmployeeClockRecord(savePath, selectedName);
+            Console.WriteLine(selectedName);
+        }
+
+
+    }
+}
